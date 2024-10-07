55d7f7accf92255d3806418d48d1f2ba2c2f193e
ritik7739 <ritik.kumar7739@gmail.com>
2024-10-07 18:30:53 +0530

    Add post login Page functionality

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport=require("passport");
-const saveRedirectUrl=require("../middleware.js")
+const { saveRedirectUrl }=require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
